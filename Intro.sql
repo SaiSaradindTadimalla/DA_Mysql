1602101d105ac114ab24d387d212fc6766f6779a
SaiSaradindTadimalla <34211454+SaiSaradindTadimalla@users.noreply.github.com>
2024-02-12 21:54:40 +0530

    Add files via upload

---
+++ Intro.sql
@@ -0,0 +1,36 @@
+/*
+About the Project File:
+
+Domain : E-Commerce
+Project Name: Olist Store Analysis
+Dataset Name: Total 9 files
+Dataset Type: CSV Data
+Dataset Zip Folder Size: 45 MB
+
+Given Number of Datasets: 09
+
+Given KPI's:
+
+Weekday Vs Weekend (order_purchase_timestamp) Payment Statistics
+Number of Orders with review score 5 and payment type as credit card.
+Average number of days taken for order_delivered_customer_date for pet_shop
+Average price and payment values from customers of sao paulo city
+Relationship between shipping days (order_delivered_customer_date - order_purchase_timestamp) Vs review scores.
+
+
+
+
+
+Steps Followed while Importing data from excel to MySQL Database:
+
+-- Creates a Schema 
+-- Right click on the Schema and select the "Table Data Import Wizard"
+-- Import a Excel Sheet with the extention (.csv)
+
+*/
+
+-- Database Name : WW
+-- Table Name : allinone
+
+use WW;
+select * from allinone;
